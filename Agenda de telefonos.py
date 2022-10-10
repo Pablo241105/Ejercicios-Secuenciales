e043b760c6825bcb0be33ffdcd5b6bd75ac7541e
Pablo <al_prodriguezf@iessierradelasvillas.org>
2022-10-10 11:16:10 +0200

    Ejercicio de clase del 10-10-2022

---
+++ Agenda de telefonos.py
@@ -0,0 +1,16 @@
+'''
+Opción 1:
+Lista para nombres
+Lista para telefonos
+Opción 2:
+Lista para nombres y telefonos
+Ejemplo[Juan - Telefono,Pepe- Telefono]
+'''
+#Opción 1
+vNombres = []
+vTelefonos = []
+
+vNombres.append(input("Dime un nombre: "))
+vNombres.append(input("Dime un numero: "))
+print(vNombres)
+print(vTelefonos)
