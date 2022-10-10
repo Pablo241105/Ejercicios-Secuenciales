3452f3d100e26c18918b9b873ab39b6052249308
Pablo <al_prodriguezf@iessierradelasvillas.org>
2022-10-10 10:40:03 +0200

    cambios

--- Ejercicios Vol XX.py
+++ Ejercicios Vol XX.py
@@ -5,15 +5,15 @@ monedade20cent=0
 monedade10cent=0
 
 
-monedadedos=(int)(input("Cuantas monedas de 2€"))
-monedadeuno=(int)(input("Cuantas monedas de 1€"))
-monedade50cent=(int)(input("Cuantas monedas de 50cent"))
-monedade20cent=(int)(input("Cuantas monedas de 20cent"))
-monedade10cent=(int)(input("Cuantas monedas de 10cent"))
+monedadedos=(int)(input("Cuantas monedas de 2€ :\n"))
+monedadeuno=(int)(input("Cuantas monedas de 1€:\n"))
+monedade50cent=(int)(input("Cuantas monedas de 50cent:\n"))
+monedade20cent=(int)(input("Cuantas monedas de 20cent:\n"))
+monedade10cent=(int)(input("Cuantas monedas de 10cent:\n"))
 monedasdeeuro=monedadedos*2+monedadeuno
 monedasdecentimos=monedade50cent*50+monedade20cent*20+monedade10cent*10
 
 print("Tienes estos euros",monedasdeeuro,"Tienes estos centimos",monedasdecentimos)
 if(monedasdecentimos>=100) :
-    print("Sumando los centimos te quedan estos euros",monedasdeeuro+1,"Y estos centimos" ,monedasdecentimos-100)
+    print("Sumando los centimos te quedan estos euros ",monedasdeeuro+1,"Y estos centimos " ,monedasdecentimos-100)
     
